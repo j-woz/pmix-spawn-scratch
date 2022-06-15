d8315e80839102c18844a8d9870045952525322b
Justin Wozniak <woz@anl.gov>
2022-06-15 14:16:32 -0500

    Better sample MPI program

--- mpi-1.c
+++ mpi-1.c
@@ -1,14 +1,19 @@
 
-
+#include <stdio.h>
 
 #include <mpi.h>
 
-#include <stdio.h>
-
-int main()
+int
+main()
 {
   printf("HELLO\n");
   MPI_Init(NULL, NULL);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0)
+    printf("rank: %i/%i\n", rank, size);
   MPI_Finalize();
+  printf("BYE\n");
   return 0;
 }
